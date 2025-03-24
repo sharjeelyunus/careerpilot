44bcd7b56a91eae0c773634215bde70a443991a6
Sharjeel Yunus <sharjeel924@gmail.com>
2025-03-24 10:42:45 +0500

    Fix typo in homepage heading; update "Interview-Read" to "Interview-Ready"

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -24,7 +24,7 @@ const HomePage = async () => {
     <>
       <section className='card-cta'>
         <div className='flex flex-col gap-6 max-w-lg'>
-          <h2>Get Interview-Read with AI-Powered Practice & Feedback</h2>
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
           <p className='text-lg'>
             Practice on read interview questions & get instant feedback
           </p>
