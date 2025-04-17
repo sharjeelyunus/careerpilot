5f6b1aa5588ed0774152d3d76172ae7557038934
Sharjeel Yunus <sharjeel924@gmail.com>
2025-04-17 07:33:19 +0500

    Add footer with attribution link in RootLayout component
    - Introduced a footer section in the RootLayout component to provide attribution.
    - Added a link to the GitHub profile of the developer, enhancing transparency and credit.
    - Ensured the footer is styled appropriately for a consistent user experience.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Mona_Sans } from 'next/font/google';
 import './globals.css';
 import { Providers } from '@/components/providers';
+import Link from 'next/link';
 
 const monaSans = Mona_Sans({
   variable: '--font-mona-sans',
@@ -16,12 +17,25 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang='en' suppressHydrationWarning>
       <body className={`${monaSans.className} antialiased pattern`}>
         <Providers>{children}</Providers>
+        {/* Footer */}
+        <div className='mt-auto py-8'>
+          <p className='text-center text-sm text-light-100/50'>
+            Built with ❤️ by{' '}
+            <Link
+              href='https://github.com/sharjeelyunus'
+              target='_blank'
+              className='text-primary-200 hover:underline'
+            >
+              Sharjeel Yunus
+            </Link>
+          </p>
+        </div>
       </body>
     </html>
   );
