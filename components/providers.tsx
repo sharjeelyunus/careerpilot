8c74d3526f910d2a207a9c862d6f0684dbb21df9
Sharjeel Yunus <sharjeel924@gmail.com>
2025-04-29 23:26:48 +0500

    Update ThemeProvider default theme and system settings in Providers component
    - Changed the default theme from 'system' to 'dark' and disabled the system theme option in the ThemeProvider for improved user experience.

--- components/providers.tsx
+++ components/providers.tsx
@@ -7,7 +7,7 @@ import { ErrorBoundary } from '@/components/error-boundary'
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ErrorBoundary component="Root">
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
         {children}
         <Toaster position="top-right" />
       </ThemeProvider>
